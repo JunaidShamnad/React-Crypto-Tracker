52b92ba8942bd3fb1ab958db8282068eb5dda276
Junaid S <intelpik@gmail.com>
2021-01-21 12:38:21 +0530

    mapping through api data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
+import Coin from "./Coin";
 
 function App() {
   const [coins, setCoins] = useState([]);
-  const[search,setSearch] = useState('')
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     axios
@@ -17,22 +18,41 @@ function App() {
       .catch((error) => console.log(error));
   }, []);
 
-  const handleChange =e=>{
-    setSearch(e.target.value)
-  }
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
 
-  const filteredCoins = coins.filter(coin=>
+  const filteredCoins = coins.filter((coin) =>
     coin.name.toLowerCase().includes(search.toLowerCase())
-    )
+  );
 
   return (
     <div className="coin-app">
       <div className="coin-search">
         <h1 className="coin-text">Search a currency</h1>
         <form>
-          <input type="text" placeholder="Search" className="coin-input" onChange={handleChange} />
+          <input
+            type="text"
+            placeholder="Search"
+            className="coin-input"
+            onChange={handleChange}
+          />
         </form>
       </div>
+      {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            volume={coin.market_cap}
+            price={coin.current_price}
+            priceChange={coin.price_change_percentage_24h}
+            marketcap={coin.}
+          />
+        );
+      })}
     </div>
   );
 }
--- src/Coin.js
+++ src/Coin.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Coin = ({name,image,symbol,price,volume}) => {
+const Coin = ({ name, image, symbol, price, volume,priceChange,marketcap }) => {
   return (
     <div className="coin-container">
       <div className="coin-row">
@@ -12,6 +12,18 @@ const Coin = ({name,image,symbol,price,volume}) => {
         <div className="coin-data">
           <p className="coin-price">${price}</p>
           <p className="coin-volume">${volume.toLocaleString()}</p>
+          {priceChange < 0 ? (
+              <p className="coin-percent red">
+                  {priceChange.toFixed(2)}%
+              </p>
+          ) : (
+             <p className="coin-percent green">
+                 {priceChange.toFixed(2)}%
+             </p>
+          )}
+          <p className="coin-marketcap">
+              Mkt Cap:${marketcap.toLocaleString()}
+          </p>
         </div>
       </div>
     </div>
